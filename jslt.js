8f42fa09be48d89bec2aaed4d3302f18cfee94f1
gitjslt <72376253+gitjslt@users.noreply.github.com>
2021-05-23 16:28:57 +0800

    Add files via upload

---
+++ jslt.js
@@ -0,0 +1,15 @@
+function getRTime() {
+    var EndTime = new Date("2021/05/23 15:00:00"); 
+    var NowTime = new Date();
+    var t = NowTime.getTime() - EndTime.getTime();
+    var d = Math.floor(t / 1000 / 60 / 60 / 24);
+    var h = Math.floor(t / 1000 / 60 / 60 % 24);
+    var m = Math.floor(t / 1000 / 60 % 60);
+    var s = Math.floor(t / 1000 % 60);
+ 
+    document.getElementById("d").innerHTML = d + " 天";
+    document.getElementById("h").innerHTML = h + " 时";
+    document.getElementById("m").innerHTML = m + " 分";
+    document.getElementById("s").innerHTML = s + " 秒";
+}
+setInterval(getRTime, 1000);
